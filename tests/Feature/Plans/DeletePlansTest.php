841360f871e7f0022b9f912e6ada57a1e9ccfb9c
Xochy Chavez Deather Ayala <xochydeather@gmail.com>
2024-04-08 16:08:45 -0600

    Plans
    Se agregan test basicos para el modulo de plans.

--- tests/Feature/Plans/DeletePlansTest.php
+++ tests/Feature/Plans/DeletePlansTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Plans;
 
+use App\Models\Plan;
 use App\Models\User;
+use Database\Seeders\permissionsSeeders\PlansPermissionsSeeder;
 use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Spatie\Permission\Models\Role;
@@ -12,6 +14,9 @@ class DeletePlansTest extends TestCase
 {
     use RefreshDatabase;
 
+    const MODEL_PLURAL_NAME = 'plans';
+    const MODEL_MAIN_ACTION_ROUTE = 'v1.'. self::MODEL_PLURAL_NAME .'.destroy';
+
     protected User $user;
 
     public function setUp(): void
@@ -20,14 +25,39 @@ class DeletePlansTest extends TestCase
 
         if (!Role::whereName('admin')->exists()) {
             $this->seed(RoleSeeder::class);
+            $this->seed(PlansPermissionsSeeder::class);
         }
 
         $this->user = User::factory()->create()->assignRole('admin');
     }
 
     /** @test */
-    public function it_can_test()
+    public function guests_users_cannot_delete_plans()
     {
-        //
+        $plan = Plan::factory()->create();
+
+        $response = $this->jsonApi()
+            ->delete(route(self::MODEL_MAIN_ACTION_ROUTE, $plan->getRouteKey()));
+
+        // Unauthorized (401)
+        $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function authenticated_users_as_admin_can_delete_plans()
+    {
+        $plan = Plan::factory()->create();
+
+        $response = $this->actingAs($this->user)
+            ->jsonApi()
+            ->delete(route(self::MODEL_MAIN_ACTION_ROUTE, $plan->getRouteKey()));
+
+        // No Content (204)
+        $response->assertStatus(204);
+        $response->assertNoContent();
+
+        $this->assertDatabaseMissing(self::MODEL_PLURAL_NAME, [
+            'id' => $plan->getKey()
+        ]);
     }
 }
